64485c3204a32aa67554f37c0db408d39323966c
Dimitri Harding <vaughnharding@hotmail.com>
2021-07-24 15:24:41 -0500

    refactor: completed email confirmation functionality

--- src/pages/api/newsletter.js
+++ src/pages/api/newsletter.js
@@ -1,7 +1,37 @@
 import sgClient from '@sendgrid/client'
+import sgMail from '@sendgrid/mail'
 import requireAuth from './_require-auth'
 
 sgClient.setApiKey(process.env.SENDGRID_API_KEY)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const env = process.env.NODE_ENV || 'development'
+
+const sendConfirmationEmail = (userInfo) => {
+  return new Promise((resolve, reject) => {
+    const message = {
+      to: userInfo.to,
+      from: 'dvhharding@gmail.com',
+      templateId: 'd-ec3f3f84edf640b5853558facc086e7e',
+      dynamic_template_data: {
+        verification_link: `${
+          env === 'production'
+            ? 'https://dimitriharding.com'
+            : 'http://localhost:3000'
+        }/confirm-email?email=${userInfo.to}`,
+        name: userInfo.name,
+      },
+    }
+    return sgMail
+      .send(message)
+      .then((response) => {
+        return resolve(response)
+      })
+      .catch((error) => {
+        return reject(error)
+      })
+  })
+}
 
 export default requireAuth((req, res) => {
   const { email, firstName, lastName } = req.body
@@ -39,8 +69,20 @@ export default requireAuth((req, res) => {
 
   return sgClient
     .request(verified ? verifyEmailOptions : options)
-    .then(() => {
-      return res.status(200).json({ status: 'success' })
+    .then(async () => {
+      if (verified) {
+        return res.status(200).json({ status: 'success' })
+      } else {
+        try {
+          const response = await sendConfirmationEmail({
+            to: email,
+            name: firstName,
+          })
+          return res.status(200).json({ status: 'success', response })
+        } catch (error) {
+          return res.status(500).json({ status: 'error', error })
+        }
+      }
     })
     .catch((error) => {
       console.log('newsletter error', JSON.stringify(error, null, '\t'))
